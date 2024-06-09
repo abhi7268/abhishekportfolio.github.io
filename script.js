f536df7fae3770ca6a752872bb636801368e0470
Abhishek varma <abhi726896@gmail.com>
2024-06-09 12:43:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Typed.js for dynamic text
+var typed = new Typed('.typed', {
+    strings: ["Web Developer.", "Designer.", "C++ Enthusiast."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+// ScrollReveal for reveal animations
+ScrollReveal().reveal('#about', { delay: 200 });
+ScrollReveal().reveal('#skills', { delay: 200 });
+ScrollReveal().reveal('#education', { delay: 200 });
+ScrollReveal().reveal('#projects', { delay: 200 });
+ScrollReveal().reveal('#contact', { delay: 200 });
